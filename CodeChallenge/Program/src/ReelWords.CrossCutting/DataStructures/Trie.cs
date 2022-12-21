76ec91823782a4e799e6734c0e42038efba7177c
Raul <rulyotano@gmail.com>
2022-12-21 22:39:41 +0100

    implemented Trie data structure

--- CodeChallenge/Program/src/ReelWords.CrossCutting/DataStructures/Trie.cs
+++ CodeChallenge/Program/src/ReelWords.CrossCutting/DataStructures/Trie.cs
@@ -1,21 +1,100 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 namespace ReelWords.CrossCutting.DataStructures;
 
 public class Trie
 {
-  public bool Search(string s)
-  {
-    throw new NotImplementedException();
-  }
+    private readonly char? _key;
+    private readonly IDictionary<char, Trie> _children;
+    private readonly Trie _parent;
+    private bool _isMatch;
 
-  public void Insert(string s)
-  {
-    throw new NotImplementedException();
-  }
+    public Trie()
+    {
+        _key = null;
+        _children = new Dictionary<char, Trie>();
+        _isMatch = false;
+        _parent = null;
+    }
 
-  public void Delete(string s)
-  {
-    throw new NotImplementedException();
-  }
+    private Trie(char key, Trie parent) : this()
+    {
+        _key = key;
+        _parent = parent;
+    }
+
+    public bool Search(string searchValue) => GetNode(this, searchValue)?._isMatch == true;
+
+    public void Insert(string newValue)
+    {
+        if (string.IsNullOrEmpty(newValue)) return;
+        int currentIndex = 0;
+        var current = this;
+
+        while (currentIndex < newValue.Length)
+        {
+            var currentCharacter = newValue[currentIndex];
+            if (!current._children.TryGetValue(currentCharacter, out var child))
+            {
+                child = GetChildNode(currentCharacter, current);
+                current._children.Add(currentCharacter, child);
+            }
+            current = child;
+            currentIndex++;
+        }
+
+        current.SetMatch();
+    }
+
+    public void Delete(string removeValue)
+    {
+        var trie = GetNode(this, removeValue);
+        if (trie == null) return;
+        trie.RemoveMatch();
+        RemoveEmptyNodes(trie);
+    }
+
+    private bool IsLeaf => _children.Count == 0;
+
+    private void SetMatch() => _isMatch = true;
+
+    private void RemoveMatch() => _isMatch = false;
+
+    private static Trie GetChildNode(char key, Trie parent) => new (key, parent);
+
+    private static Trie GetNode(Trie root, string value)
+    {
+        if (string.IsNullOrEmpty(value)) return null;
+
+        var currentIndex = 0;
+        var current = root;
+
+        while (currentIndex < value.Length)
+        {
+            var currentCharacter = value[currentIndex];
+            if (!current._children.TryGetValue(currentCharacter, out var child))
+            {
+                return null;
+            }
+
+            current = child;
+            currentIndex++;
+        }
+
+        return current;
+    }
+
+    private static void RemoveEmptyNodes(Trie trie)
+    {
+        var current = trie;
+        var parent = current._parent;
+
+        while (parent != null)
+        {
+            if (!current.IsLeaf || current._isMatch) break;
+            parent._children.Remove(current._key.Value);
+            current = parent;
+            parent = current._parent;
+        }
+    }
 }
--- CodeChallenge/Program/tests/ReelWords.CrossCutting.Tests/DataStructuresTests/TrieTests.cs
+++ CodeChallenge/Program/tests/ReelWords.CrossCutting.Tests/DataStructuresTests/TrieTests.cs
@@ -4,23 +4,144 @@ namespace ReelWords.CrossCutting.Tests.DataStructuresTests;
 
 public class TrieTests
 {
-    private const string AWESOME_CO = "pierplay";
-
     [Fact]
-    public void TrieInsertTest()
+    public void When_InsertNewValueAndSearchIt_Should_ReturnTrue()
     {
-        Trie trie = new Trie();
-        trie.Insert(AWESOME_CO);
-        Assert.True(trie.Search(AWESOME_CO));
+        var trie = GetTrie();
+        const string value = "abc";
+        trie.Insert(value);
+
+        Assert.True(trie.Search(value));
     }
 
     [Fact]
-    public void TrieDeleteTest()
+    public void When_InsertNullOrEmpty_Should_NotRaiseErrorAndSearchReturnFalse()
     {
-        Trie trie = new Trie();
-        trie.Insert(AWESOME_CO);
-        Assert.True(trie.Search(AWESOME_CO));
-        trie.Delete(AWESOME_CO);
-        Assert.True(trie.Search(AWESOME_CO));
+        var trie = GetTrie();
+        trie.Insert(null);
+
+        Assert.False(trie.Search(null));
     }
+
+    [Fact]
+    public void When_SearchNotExistingNode_ShouldReturnFalse()
+    {
+        var trie = GetTrie();
+        const string value = "abc";
+        trie.Insert(value);
+
+        Assert.False(trie.Search("not-existing-key"));
+    }
+
+    [Fact]
+    public void When_SearchIntermediateNodeValue_Should_ReturnFalse()
+    {
+        var trie = GetTrie();
+        const string value = "abc";
+        trie.Insert(value);
+
+        Assert.False(trie.Search("ab"));
+    }
+
+    [Fact]
+    public void Should_InsertSeveralTimesSameValue()
+    {
+        var trie = GetTrie();
+        const string value = "abc";
+        trie.Insert(value);
+        trie.Insert(value);
+        trie.Insert(value);
+
+        Assert.True(trie.Search(value));
+    }
+
+    [Fact]
+    public void Should_InsertAndSearchCorrectlySeveralValues()
+    {
+        var trie = GetTrie();
+        const string value1 = "abc";
+        const string value2 = "abcde";
+        trie.Insert(value1);
+        trie.Insert(value2);
+
+        Assert.True(trie.Search(value1));
+        Assert.True(trie.Search(value2));
+    }
+
+    [Fact]
+    public void When_AddingValueInsideAlreadyExistingPath_Should_InsertAndSearchCorrectly()
+    {
+        var trie = GetTrie();
+        const string fullValue = "abcde";
+        const string newValue = "abc";
+        trie.Insert(fullValue);
+        trie.Insert(newValue);
+
+        Assert.True(trie.Search(fullValue));
+        Assert.True(trie.Search(newValue));
+        Assert.False(trie.Search("abcd"));
+    }
+
+    [Fact]
+    public void When_AddingValueExtendingAlreadyExistingPath_Should_InsertAndSearchCorrectly()
+    {
+        var trie = GetTrie();
+        const string oldSmallValue = "abc";
+        const string newValue = "abcde";
+        trie.Insert(oldSmallValue);
+        trie.Insert(newValue);
+
+        Assert.True(trie.Search(oldSmallValue));
+        Assert.True(trie.Search(newValue));
+        Assert.False(trie.Search("abcd"));
+    }
+
+    [Fact]
+    public void When_Deleting_Should_RemoveFromItemFromResults()
+    {
+        var trie = GetTrie();
+        const string value = "abcd";
+        trie.Insert(value);
+
+        Assert.True(trie.Search(value));
+
+        trie.Delete(value);
+        Assert.False(trie.Search(value));
+    }
+
+    [Fact]
+    public void When_DeletingLongest_Should_KeepOtherExistingValues()
+    {
+        var trie = GetTrie();
+        const string value1 = "abcd";
+        const string value2 = "abcde";
+        trie.Insert(value1);
+        trie.Insert(value2);
+
+        Assert.True(trie.Search(value1));
+        Assert.True(trie.Search(value2));
+
+        trie.Delete(value2);
+        Assert.False(trie.Search(value2));
+        Assert.True(trie.Search(value1));
+    }
+
+    [Fact]
+    public void When_DeletingShortest_Should_KeepOtherExistingValues()
+    {
+        var trie = GetTrie();
+        const string value1 = "abcd";
+        const string value2 = "abcde";
+        trie.Insert(value1);
+        trie.Insert(value2);
+
+        Assert.True(trie.Search(value1));
+        Assert.True(trie.Search(value2));
+
+        trie.Delete(value1);
+        Assert.False(trie.Search(value1));
+        Assert.True(trie.Search(value2));
+    }
+
+    private Trie GetTrie() => new();
 }
